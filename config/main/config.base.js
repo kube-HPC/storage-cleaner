5929b4ce321b857108b78af9267252914cc2bc03
bahalool <bahalool@gmail.com>
2018-12-02 14:26:02 +0200

    env for fs adapter

--- config/main/config.base.js
+++ config/main/config.base.js
@@ -25,7 +25,7 @@ config.s3 = {
 };
 
 config.fs = {
-    baseDirectory: process.env.FS_DIRECTORY || ''
+    baseDirectory: process.env.BASE_FS_ADAPTER_DIRECTORY || '/var/tmp/fs/storage'
 };
 
 config.storageAdapters = {
